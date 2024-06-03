9d52796726ef88842d333a6b8762d625605d0bce
rorro <rodguzm03@gmail.com>
2024-06-03 00:13:45 -0400

    cambios en html´s

---
+++ ASSETS/js/peticion/getUsuarios.js
@@ -0,0 +1,19 @@
+export async function getUsuarios() {
+    try{
+
+        const response = await fetch('https://proyecto-api-usuarios.onrender.com');
+
+        if (!response.ok) {
+            
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return data.Usuarios;
+        
+    } catch(error){
+        console.error('Error:', error);
+        throw error;
+    }
+}
